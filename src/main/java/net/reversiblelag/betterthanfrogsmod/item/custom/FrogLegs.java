9dc825bf32128f6bbd65b3686d035413bd57db86
ReversibleLag <kaisouth123@gmail.com>
2022-07-28 18:28:50 -0400

    still adding frog legs

---
+++ src/main/java/net/reversiblelag/betterthanfrogsmod/item/custom/FrogLegs.java
@@ -0,0 +1,27 @@
+package net.reversiblelag.betterthanfrogsmod.item.custom;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+public class FrogLegs extends Item {
+
+
+    public FrogLegs(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if(!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
+            //Output a random number
+            //set a cooldown
+        }
+
+        return super.use(level, player, hand);
+    }
+}
